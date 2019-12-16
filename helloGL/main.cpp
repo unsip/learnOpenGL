13e1884e6ded8c5464ce52915893a00aa7e9fda4
unsip <unsip@protonmail.com>
2019-12-16 22:29:04 +0300

    Working triangles!

--- helloGL/main.cpp
+++ helloGL/main.cpp
@@ -65,36 +65,81 @@ int main()
     glViewport(0, 0, DX, DY);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+    };
+    GLuint indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    // Initialize and bind Vertex Array Object (VAO), Vertex Buffer Object
+    // (VBO), Elements Buffer Object (EBO).
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    const char* vertData = Shaders::Vert.data();
+    glShaderSource(vs, 1, &vertData, nullptr);
+    glCompileShader(vs);
+    printCompileStatus(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* fragData = Shaders::Frag.data();
+    glShaderSource(fs, 1, &fragData, nullptr);
+    glCompileShader(fs);
+    printCompileStatus(fs);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
+    glLinkProgram(shaderProgram);
+
+    // Free memory from shader objects left by glCreateShader().
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Draw in wireframe mode.
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     while(!glfwWindowShouldClose(window))
     {
         processInput(window);
 
-        glClearColor(0.7f, 0.2f, 0.3f, 0.1f);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
 
-        // Array of vertices for triangle.
-        float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-             0.5f, -0.5f, 0.0f,
-             0.5f,  0.5f, 0.0f,
-        };
-
-        // Initialize and bind Vertex Buffer Object
-        GLuint VBO;
-        glGenBuffers(1, &VBO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-        const char* vertData = Shaders::Vert.data();
-        glShaderSource(vs, 1, &vertData, nullptr);
-        glCompileShader(vs);
-        printCompileStatus(vs);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    // Free memory from buffer objects.
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &VBO);
     glfwTerminate();
+
     return EXIT_SUCCESS;
 }
